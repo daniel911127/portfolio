bb1fc5636de9603ed574ef125177ce60a8033802
Daniel Moreno <daniel_9127@hotmail.com>
2023-05-16 19:29:45 -0500

    fix margin proyects

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,7 +4,7 @@ function Experience() {
     <div>
       <h3 className="text-orange-300 ml-[30%] uppercase text-3xl mb-4">proyectos</h3>
       <section className="w-full flex flex-row justify-center flex-wrap gap-px mb-6 "> 
-        <article className="w-72 h-72 flex justify-center flex-col mr-4 bg-sky-950 p-4 rounded-2xl">
+        <article className="w-72 h-72 flex justify-center flex-col m-4 bg-sky-950 p-4 rounded-2xl">
           <a href="https://latamairlines.vercel.app" target="_blank" rel="noopener noreferrer">
           <img src="/images/aerolinea.png" alt="aerolinea" />
           </a>
@@ -12,7 +12,7 @@ function Experience() {
             <p className="text-white text-center mt-4 ">vamos al código! </p>
           </a>
         </article>
-        <article className="w-72 h-72 flex justify-center flex-col bg-sky-950 p-4 rounded-2xl">
+        <article className="w-72 h-72 flex justify-center flex-col m-4 bg-sky-950 p-4 rounded-2xl">
           <a href="http://sports21.infinityfreeapp.com/?i=1" target="_blank" rel="noopener noreferrer">
           <img src="/images/sports21.png" alt="ecommerce" />
           </a>
