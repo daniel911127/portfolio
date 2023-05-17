f4bf1abdc9ef2fb1bd74a81d0eeee410cfd11fa8
Daniel Moreno <daniel_9127@hotmail.com>
2023-05-16 19:12:11 -0500

    fix images

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -6,7 +6,7 @@ function Experience() {
       <section className="w-full flex flex-row justify-center flex-wrap gap-px mb-6 "> 
         <article className="w-72 h-72 flex justify-center flex-col mr-4 bg-sky-950 p-4 rounded-2xl">
           <a href="https://latamairlines.vercel.app" target="_blank" rel="noopener noreferrer">
-          <img src="../../public/images/aerolinea.png" alt="aerolinea" />
+          <img src="/images/aerolinea.png" alt="aerolinea" />
           </a>
           <a href="https://github.com/daniel911127/latam-airline-frontend-clone" target="_blank" rel="noopener noreferrer">
             <p className="text-white text-center mt-4 ">vamos al código! </p>
@@ -14,7 +14,7 @@ function Experience() {
         </article>
         <article className="w-72 h-72 flex justify-center flex-col bg-sky-950 p-4 rounded-2xl">
           <a href="http://sports21.infinityfreeapp.com/?i=1" target="_blank" rel="noopener noreferrer">
-          <img src="../../public/images/sports21.png" alt="ecommerce" />
+          <img src="/images/sports21.png" alt="ecommerce" />
           </a>
           <a href="https://github.com/daniel911127/Sports21" target="_blank" rel="noopener noreferrer">
             <p className="text-white text-center mt-4">vamos al código! </p>
